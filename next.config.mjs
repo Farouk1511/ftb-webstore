f69aee472e6eeec79598c9d29ab68b1304630b24
Farouk <kazeem.farouk@yahoo.com>
2024-04-15 19:29:19 -0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:"image-cdn.hypb.st",
+                port:"",
+               
+            }
+        ]
+    }
+};
 
 export default nextConfig;
